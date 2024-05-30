18edce234994322059ae916529ab7fc6a4c535f5
Ubuntu <ubuntu@ip-172-31-12-73.us-west-2.compute.internal>
2024-05-30 01:28:40 +0000

    changes in test branch

--- airwise_front_end/api_sfo.py
+++ airwise_front_end/api_sfo.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
-import requests, json
-#import xml.etree.ElementTree as ET
+import requests
 
-def get_wind_temp_data(region="all", level="low", fcst="06", id="BIH"):
+
+def get_sfo_low_06(region="sfo", level="low", fcst="06"):
     base_url = "http://www.aviationweather.gov/api/data/windtemp"
 #    base_url = "https://www.aviationweather.gov/windtemp/data"
-
     params = {
         'region': region,
         'level': level,
-        'fcst': fcst,
-	    'sites':[{'id':id}]
+        'fcst': fcst
     }
     headers = {"Accept": "application/json"}
     try:
@@ -19,8 +17,8 @@ def get_wind_temp_data(region="all", level="low", fcst="06", id="BIH"):
         response.raise_for_status()  # Raise an error for bad status codes
 
         # Print the raw response content for debugging
-        # print("Response content:", response.content)
-        
+        print("Response content:", response.content)
+
         # If response content is empty or not valid JSON, handle it
         if not response.content:
             print("Empty response")
@@ -29,47 +27,115 @@ def get_wind_temp_data(region="all", level="low", fcst="06", id="BIH"):
         # Attempt to parse the JSON response
         try:
             weather_data = response.json()
-            # print(weather_data)
         except ValueError:
             print("Response is not valid JSON")
             return None
 
-        # value = weather_data.get('id')
-        # value = weather_data['sites'][0]['id']
-        # print("value",value)
-        wind_temp_data = []
-        for site in weather_data['sites']:
-            # value = site['id']
-            # print("id",value)
-            if site['id'] == id:
-                wind_temp = {
-                    "lat": site['lat'],
-                    "lon": site['lon'],
-                    "dir": site['dir'],
-                    "spd": site['spd'],
-                }
-                wind_temp_data.append(wind_temp)
-        return wind_temp_data
         # Process the weather data as needed
-        # print("Weather data:", weather_data)
-        if 'data' not in weather_data:
-            print("No data found in response")
-            return None
-                
-        # wind_temp_data = []
-        # for data_point in weather_data: #weather_data.get('data', []):
-        #     print(data_point.keys())
-        #     print(data_point)
-        #     # wind_temp = {
+        wind_temp_data = []
+        for data_point in weather_data.get('data', []):
+            wind_temp = {
+                "latitude": data_point.get("latitude"),
+                "longitude": data_point.get("longitude"),
+                "wind_speed": data_point.get("wind_speed"),
+                "wind_direction": data_point.get("wind_direction"),
+                "temperature": data_point.get("temperature")
+            }
+            wind_temp_data.append(wind_temp)
+        return wind_temp_data
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+#    except requests.exceptions.RequestException as req_err:
+ #       print(f"Request error occurred: {req_err}")
+  #  except ET.ParseError as parse_err:
+   #     print(f"XML parse error: {parse_err}")
+    return None
+def get_sfo_low_12(region="sfo", level="low", fcst="12"):
+    base_url = "http://www.aviationweather.gov/api/data/windtemp"
+#    base_url = "https://www.aviationweather.gov/windtemp/data"
+    params = {
+        'region': region,
+        'level': level,
+        'fcst': fcst
+    }
+    headers = {"Accept": "application/json"}
+    try:
+        response = requests.get(base_url, headers = headers, params=params)
+        response.raise_for_status()  # Raise an error for bad status codes
 
-        #     #     "latitude": data_point.get("latitude"),
-        #     #     "longitude": data_point.get("longitude"),
-        #     #     "wind_speed": data_point.get("wind_speed"),
-        #     #     "wind_direction": data_point.get("wind_direction"),
-        #     #     "temperature": data_point.get("temperature")
-        #     # }
-        #     wind_temp_data.append(wind_temp)
-        # return wind_temp_data
+        # Print the raw response content for debugging
+        print("Response content:", response.content)
+
+        # If response content is empty or not valid JSON, handle it
+        if not response.content:
+            print("Empty response")
+            return None
+
+        # Attempt to parse the JSON response
+        try:
+            weather_data = response.json()
+        except ValueError:
+            print("Response is not valid JSON")
+            return None
+        # Process the weather data as needed
+        wind_temp_data = []
+        for data_point in weather_data.get('data', []):
+            wind_temp = {
+                "latitude": data_point.get("latitude"),
+                "longitude": data_point.get("longitude"),
+                "wind_speed": data_point.get("wind_speed"),
+                "wind_direction": data_point.get("wind_direction"),
+                "temperature": data_point.get("temperature")
+            }
+            wind_temp_data.append(wind_temp)
+        return wind_temp_data
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+#    except requests.exceptions.RequestException as req_err:
+ #       print(f"Request error occurred: {req_err}")
+  #  except ET.ParseError as parse_err:
+   #     print(f"XML parse error: {parse_err}")
+    return None
+
+def get_sfo_low_24(region="sfo", level="low", fcst="24"):
+    base_url = "http://www.aviationweather.gov/api/data/windtemp"
+#    base_url = "https://www.aviationweather.gov/windtemp/data"
+    params = {
+        'region': region,
+        'level': level,
+        'fcst': fcst
+    }
+    headers = {"Accept": "application/json"}
+    try:
+        response = requests.get(base_url, headers = headers, params=params)
+        response.raise_for_status()  # Raise an error for bad status codes
+
+        # Print the raw response content for debugging
+        print("Response content:", response.content)
+
+        # If response content is empty or not valid JSON, handle it
+        if not response.content:
+            print("Empty response")
+            return None
+
+        # Attempt to parse the JSON response
+        try:
+            weather_data = response.json()
+        except ValueError:
+            print("Response is not valid JSON")
+            return None
+        # Process the weather data as needed
+        wind_temp_data = []
+        for data_point in weather_data.get('data', []):
+            wind_temp = {
+                "latitude": data_point.get("latitude"),
+                "longitude": data_point.get("longitude"),
+                "wind_speed": data_point.get("wind_speed"),
+                "wind_direction": data_point.get("wind_direction"),
+                "temperature": data_point.get("temperature")
+            }
+            wind_temp_data.append(wind_temp)
+        return wind_temp_data
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
 #    except requests.exceptions.RequestException as req_err:
@@ -79,10 +145,25 @@ def get_wind_temp_data(region="all", level="low", fcst="06", id="BIH"):
     return None
 
 # Example usage
-wind_temp_data = get_wind_temp_data(region="sfo", level="low", fcst="06")  # Fetch wind and temperature data for SFO region
-print("wind_temp_data",wind_temp_data)
-#if wind_temp_data:
-#    for data in wind_temp_data:
-#        print(f"Latitude: {data['latitude']}, Longitude: {data['longitude']}, "
-#              f"Wind Speed: {data['wind_speed']} kt, Wind Direction: {data['wind_direction']} degrees, "
-#              f"Temperature: {data['temperature']} C")
+wind_temp_data = get_sfo_low_06(region="sfo", level="low", fcst="06")  # Fetch wind and temperature data for SFO region
+if wind_temp_data:
+    for data in wind_temp_data:
+        print(f"Latitude: {data['latitude']}, Longitude: {data['longitude']}, "
+              f"Wind Speed: {data['wind_speed']} kt, Wind Direction: {data['wind_direction']} degrees, "
+              f"Temperature: {data['temperature']} C")
+
+# Example usage
+wind_temp_data = get_sfo_low_12(region="sfo", level="low", fcst="12")  # Fetch wind and temperature data for SFO region
+if wind_temp_data:
+    for data in wind_temp_data:
+        print(f"Latitude: {data['latitude']}, Longitude: {data['longitude']}, "
+              f"Wind Speed: {data['wind_speed']} kt, Wind Direction: {data['wind_direction']} degrees, "
+              f"Temperature: {data['temperature']} C")
+# Example usage
+wind_temp_data = get_sfo_low_24(region="sfo", level="low", fcst="24")  # Fetch wind and temperature data for SFO region
+if wind_temp_data:
+    for data in wind_temp_data:
+        print(f"Latitude: {data['latitude']}, Longitude: {data['longitude']}, "
+              f"Wind Speed: {data['wind_speed']} kt, Wind Direction: {data['wind_direction']} degrees, "
+              f"Temperature: {data['temperature']} C")
+
--- airwise_front_end/app.py
+++ airwise_front_end/app.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env python3
-# Import the necessary modules
-import os
 from flask import Flask, render_template, request, session, redirect, flash, url_for, jsonify
-from forms import LoginForm, RegisterForm
-from api_sfo import get_wind_temp_data
-from flask_login import login_user, logout_user, login_required 
+from flask_login import login_user, logout_user, login_required
 from models import db, login_manager, UserModel, load_user
-
-# Create a new Flask application instance
+from api_sfo import get_sfo_low_06, get_sfo_low_12, get_sfo_low_24
+from api_other_pac import get_wind_temp_data
+from api_obs import get_obstacle_data
+from forms import LoginForm, RegisterForm
 app = Flask(__name__)
-app.secret_key='super secret key'
+app.secret_key = 'super secret key'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./login.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 login_manager.init_app(app)
-
-#add user routine
+# Add user routine
 def add_user(email, password):
     user = UserModel(email=email)
     user.set_password(password)
@@ -25,27 +21,114 @@ def add_user(email, password):
     db.session.commit()
     return user
 
-# create database with a test user
+# Create database with a test user
 @app.before_request
 def create_db():
     db.create_all()
     if not UserModel.query.filter_by(email='abc@uw.edu').first():
         add_user('abc@uw.edu', 'password')
-
 @app.route('/home')
 def home():
     return render_template('base.html')
+#@app.route('/dashboard', methods=['GET'])
+#@login_required
+#def dashboard():
+ #   region = request.args.get('region', 'sfo')
+  #  fcst = request.args.get('fcst', '06')
+   # level = request.args.get('level', 'low')
+    
+   # app.logger.info(f'Region: {region}, Forecast: {fcst}, Level: {level}')
 
-# Define a route for the root URL ("/") that returns "Hello World"
+   # wind_temp_data = None
+   # if region in ('sfo', 'other_pac', 'all'):  # Check for valid regions
+    #    if region == 'sfo':
+     #       if fcst == '06':
+      #          wind_temp_data = get_sfo_low_06(region=region, level=level, fcst=fcst)
+       #     elif fcst == '12':
+        #        wind_temp_data = get_sfo_low_12(region=region, level=level, fcst=fcst)
+         #   elif fcst == '24':
+          #      wind_temp_data = get_sfo_low_24(region=region, level=level, fcst=fcst)
+        #elif region == 'other_pac':
+         #   wind_temp_data = get_wind_temp_data(region=region, level=level, fcst=fcst)
+       # else:  # Handle 'all' region (assuming combining data)
+            # Implement logic to combine data from both regions (if applicable)
+        #    pass  # Replace with code to combine data from SFO and other_pac
+
+   # else:
+        # No data for invalid regions
+    #    wind_temp_data = []  # Set to empty list for clarity
+
+   # obstacle_data = get_obstacle_data(bbox="40,-90,45,-85")
+
+    # Populate the context dictionary
+   # context = {
+    #    'wind_temp_data': wind_temp_data,
+     #   'obstacle_data': obstacle_data,
+      #  'region': region,
+       # 'fcst': fcst,
+       # 'level': level
+   # }
+
+   # return render_template('dashboard.html', **context)
+
 @app.route('/dashboard', methods=['GET'])
 @login_required
 def dashboard():
-    if request.method == 'GET' and 'sites' in request.args and request.args.get('sites') is not None:
-        session['sites'] = request.args.get('sites')
-    if 'sites' in session:
-        return render_template('dashboard.html', sites=get_wind_temp_data(di=session['sites']))
-    return render_template('dashboard.html')
+    region = request.args.get('region', 'sfo')
+    fcst = request.args.get('fcst', '06')
+    level = request.args.get('level', 'low')
+    
+    app.logger.info(f'Region: {region}, Forecast: {fcst}, Level: {level}')
 
+    if region == 'sfo':
+        if fcst == '06':
+            wind_temp_data = get_sfo_low_06(region=region, level=level, fcst=fcst)
+        elif fcst == '12':
+            wind_temp_data = get_sfo_low_12(region=region, level=level, fcst=fcst)
+        elif fcst == '24':
+            wind_temp_data = get_sfo_low_24(region=region, level=level, fcst=fcst)
+    elif region == 'other_pac':
+        wind_temp_data = get_wind_temp_data(region=region, level=level, fcst=fcst)
+    else:
+        pass
+        # Assuming you want to combine data from both regions if 'all' is selected
+    #    wind_temp_data = get_sfo_data(region='sfo', level=level, fcst=fcst) + get_other_pac_data(region='other_pac', level=level, fcst=fcst)
+
+    obstacle_data = get_obstacle_data(bbox="40,-90,45,-85")
+    
+    return render_template('dashboard.html', wind_temp_data=wind_temp_data, obstacle_data=obstacle_data, region=region, fcst=fcst, level=level)
+
+@app.route('/get_wind_temp_data', methods=['GET'])
+@login_required
+def get_wind_temp():
+    region = request.args.get('region', 'sfo')
+    fcst = request.args.get('fcst', '06')
+    level = request.args.get('level', 'low')
+
+    app.logger.info(f'Region: {region}, Forecast: {fcst}, Level: {level}')
+    
+    if region == 'sfo':
+        if fcst == '06':
+            data = get_sfo_low_06(region=region, level=level, fcst=fcst)
+        elif fcst == '12':
+            data = get_sfo_low_12(region=region, level=level, fcst=fcst)
+        elif fcst == '24':
+            data = get_sfo_low_24(region=region, level=level, fcst=fcst)
+    elif region == 'other_pac':
+        data = get_wind_temp_data(region=region, level=level, fcst=fcstt)
+    else:
+        pass
+        # Assuming you want to combine data from both regions if 'all' is selected
+ #       data = get_sfo_data(region='sfo', level=level, fcst=fcst) + get_other_pac_data(region='other_pac', level=level, fcst=fcstt)
+    
+    return jsonify(data)
+
+@app.route('/get_obstacle_data', methods=['GET'])
+@login_required
+def get_obstacle():
+    bbox = request.args.get('bbox', '40,-90,45,-85')
+    data = get_obstacle_data(bbox=bbox)
+    return jsonify(data)
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -62,7 +145,6 @@ def register():
         user_exists = True
         return redirect(url_for('login'))
     return render_template('register.html', form=form, user_exists=user_exists)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -83,7 +165,7 @@ def logout():
     logout_user()
     flash('You have been logged out.', 'success')
     return redirect(url_for('home'))
-
 # Run the application if this script is being run directly
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
+
