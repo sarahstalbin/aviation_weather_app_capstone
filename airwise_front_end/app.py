c577031585e23f6ff47601b57ba64f06ddcdea30
Minna <minnac@Minnas-MBP.lan>
2024-05-30 17:48:03 -0700

    works for map and graph

--- airwise_front_end/app.py
+++ airwise_front_end/app.py
@@ -1,19 +1,28 @@
+#!/usr/bin/env python3
+# Import the necessary modules
+import os
 from flask import Flask, render_template, request, session, redirect, flash, url_for, jsonify
-from flask_login import login_user, logout_user, login_required
-from models import db, login_manager, UserModel, load_user
-from api_sfo import get_sfo_low_06, get_sfo_low_12, get_sfo_low_24
-from api_other_pac import get_wind_temp_data
-from api_obs import get_obstacle_data
+from datetime import datetime
 from forms import LoginForm, RegisterForm
+from api_sfo import get_wind_temp_data
+from map import inject_tile_layer
+from plot_data import plotting
+import plotly.graph_objects as go
+from flask_login import login_user, logout_user, login_required 
+from models import db, login_manager, UserModel, load_user
+from flask import request
+
+# Create a new Flask application instance
 app = Flask(__name__)
-app.secret_key = 'super secret key'
+app.secret_key='super secret key'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./login.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 login_manager.init_app(app)
-# Add user routine
+
+#add user routine
 def add_user(email, password):
     user = UserModel(email=email)
     user.set_password(password)
@@ -21,114 +30,60 @@ def add_user(email, password):
     db.session.commit()
     return user
 
-# Create database with a test user
+# create database with a test user
 @app.before_request
 def create_db():
     db.create_all()
     if not UserModel.query.filter_by(email='abc@uw.edu').first():
         add_user('abc@uw.edu', 'password')
+
 @app.route('/home')
 def home():
     return render_template('base.html')
-#@app.route('/dashboard', methods=['GET'])
-#@login_required
-#def dashboard():
- #   region = request.args.get('region', 'sfo')
-  #  fcst = request.args.get('fcst', '06')
-   # level = request.args.get('level', 'low')
+
+
+@app.route('/map')
+def index():
+    return render_template('map.html')
+  
+
+@app.route('/plot', methods=['GET'])
+def plot():
+    # print("I am in plot")
+
+    if request.method == 'GET' and 'data_state' in request.args and request.args.get('data_state') is not None:
+        session['data_state'] = request.args.get('data_state')
+        print("data_state", session['data_state'])
+    if request.method == 'GET' and 'city' in request.args and request.args.get('city') is not None:
+        session['city'] = request.args.get('city')
+    if request.method == 'GET' and 'weather' in request.args and request.args.get('weather') is not None:
+        session['weather'] = request.args.get('weather')
+    if 'data_state' in session:
+        past_type = session.get('data_state')
+    else:
+        past_type = 'past'
     
-   # app.logger.info(f'Region: {region}, Forecast: {fcst}, Level: {level}')
+    if 'city' in session:
+        city_type = session.get('city')
+    else:
+        city_type = 'Seattle'
 
-   # wind_temp_data = None
-   # if region in ('sfo', 'other_pac', 'all'):  # Check for valid regions
-    #    if region == 'sfo':
-     #       if fcst == '06':
-      #          wind_temp_data = get_sfo_low_06(region=region, level=level, fcst=fcst)
-       #     elif fcst == '12':
-        #        wind_temp_data = get_sfo_low_12(region=region, level=level, fcst=fcst)
-         #   elif fcst == '24':
-          #      wind_temp_data = get_sfo_low_24(region=region, level=level, fcst=fcst)
-        #elif region == 'other_pac':
-         #   wind_temp_data = get_wind_temp_data(region=region, level=level, fcst=fcst)
-       # else:  # Handle 'all' region (assuming combining data)
-            # Implement logic to combine data from both regions (if applicable)
-        #    pass  # Replace with code to combine data from SFO and other_pac
+    if 'weather' in session:
+        weather_type = session.get('weather')
+    else:
+        weather_type = 'temp'
+    return render_template('plot.html', graph_html=plotting(location=city_type, type=weather_type, current_state=past_type))  
 
-   # else:
-        # No data for invalid regions
-    #    wind_temp_data = []  # Set to empty list for clarity
-
-   # obstacle_data = get_obstacle_data(bbox="40,-90,45,-85")
-
-    # Populate the context dictionary
-   # context = {
-    #    'wind_temp_data': wind_temp_data,
-     #   'obstacle_data': obstacle_data,
-      #  'region': region,
-       # 'fcst': fcst,
-       # 'level': level
-   # }
-
-   # return render_template('dashboard.html', **context)
-
+# Define a route for the root URL ("/") that returns "Hello World"
 @app.route('/dashboard', methods=['GET'])
 @login_required
 def dashboard():
-    region = request.args.get('region', 'sfo')
-    fcst = request.args.get('fcst', '06')
-    level = request.args.get('level', 'low')
-    
-    app.logger.info(f'Region: {region}, Forecast: {fcst}, Level: {level}')
+    if request.method == 'GET' and 'sites' in request.args and request.args.get('sites') is not None:
+        session['sites'] = request.args.get('sites')
+    if 'sites' in session:
+        return render_template('dashboard.html', sites=get_wind_temp_data(di=session['sites']))
+    return render_template('dashboard.html')
 
-    if region == 'sfo':
-        if fcst == '06':
-            wind_temp_data = get_sfo_low_06(region=region, level=level, fcst=fcst)
-        elif fcst == '12':
-            wind_temp_data = get_sfo_low_12(region=region, level=level, fcst=fcst)
-        elif fcst == '24':
-            wind_temp_data = get_sfo_low_24(region=region, level=level, fcst=fcst)
-    elif region == 'other_pac':
-        wind_temp_data = get_wind_temp_data(region=region, level=level, fcst=fcst)
-    else:
-        pass
-        # Assuming you want to combine data from both regions if 'all' is selected
-    #    wind_temp_data = get_sfo_data(region='sfo', level=level, fcst=fcst) + get_other_pac_data(region='other_pac', level=level, fcst=fcst)
-
-    obstacle_data = get_obstacle_data(bbox="40,-90,45,-85")
-    
-    return render_template('dashboard.html', wind_temp_data=wind_temp_data, obstacle_data=obstacle_data, region=region, fcst=fcst, level=level)
-
-@app.route('/get_wind_temp_data', methods=['GET'])
-@login_required
-def get_wind_temp():
-    region = request.args.get('region', 'sfo')
-    fcst = request.args.get('fcst', '06')
-    level = request.args.get('level', 'low')
-
-    app.logger.info(f'Region: {region}, Forecast: {fcst}, Level: {level}')
-    
-    if region == 'sfo':
-        if fcst == '06':
-            data = get_sfo_low_06(region=region, level=level, fcst=fcst)
-        elif fcst == '12':
-            data = get_sfo_low_12(region=region, level=level, fcst=fcst)
-        elif fcst == '24':
-            data = get_sfo_low_24(region=region, level=level, fcst=fcst)
-    elif region == 'other_pac':
-        data = get_wind_temp_data(region=region, level=level, fcst=fcstt)
-    else:
-        pass
-        # Assuming you want to combine data from both regions if 'all' is selected
- #       data = get_sfo_data(region='sfo', level=level, fcst=fcst) + get_other_pac_data(region='other_pac', level=level, fcst=fcstt)
-    
-    return jsonify(data)
-
-@app.route('/get_obstacle_data', methods=['GET'])
-@login_required
-def get_obstacle():
-    bbox = request.args.get('bbox', '40,-90,45,-85')
-    data = get_obstacle_data(bbox=bbox)
-    return jsonify(data)
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -145,6 +100,7 @@ def register():
         user_exists = True
         return redirect(url_for('login'))
     return render_template('register.html', form=form, user_exists=user_exists)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -165,7 +121,7 @@ def logout():
     logout_user()
     flash('You have been logged out.', 'success')
     return redirect(url_for('home'))
+
 # Run the application if this script is being run directly
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
-
